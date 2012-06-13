e2b297b541f9e105747c437d96a011588eebeef1
sibjewish@gmail.com <Kenny@Kenny-.(none)>
2012-06-13 16:41:28 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+﻿<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+	<title>Main</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<link rel="stylesheet" type="text/css" href="/css/style.css">
+</head>
+<body>
+	<p>
+		<h2>It's main page :3</h2>
+	</p>
+     
+    
+</body>
+</html>
